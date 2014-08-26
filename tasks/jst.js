0f4cf109604c00425b379e9472d5994be470c95e
Ronnie <agustine103@gmail.com>
2014-08-26 17:48:46 +0800

    -

--- tasks/jst.js
+++ tasks/jst.js
@@ -9,85 +9,134 @@
 'use strict';
 var _ = require('lodash');
 var chalk = require('chalk');
+var path = require('path');
 
-module.exports = function(grunt) {
-  // filename conversion for templates
-  var defaultProcessName = function(name) { return name; };
+module.exports = function (grunt) {
+    // filename conversion for templates
+    var defaultProcessName = function (name) {
+        return name;
+    };
 
-  grunt.registerMultiTask('jst', 'Compile underscore templates to JST file', function() {
-    var lf = grunt.util.linefeed;
-    var lib = require('./lib/jst');
-    var options = this.options({
-      namespace: 'JST',
-      templateSettings: {},
-      processContent: function (src) { return src; },
-      separator: lf + lf
-    });
 
-    // assign filename transformation functions
-    var processName = options.processName || defaultProcessName;
 
-    var nsInfo;
-    if (options.namespace !== false) {
-      nsInfo = lib.getNamespaceDeclaration(options.namespace);
-    }
+    grunt.registerMultiTask('jst', 'Compile underscore templates to JST file', function () {
+        var lf = grunt.util.linefeed;
+        var lib = require('./lib/jst');
+        var files = this.files;
+        var options = this.options({
+            namespace: 'JST',
+            amd: false,
+            cmd: false,
+            processTemplateName: function(fileName){
+                var extName = path.extname(fileName);
+                return path.basename(fileName, extName);
+            },
+            templateSettings: {
+                evaluate: /<%([\s\S]+?)%>/g,
+                interpolate: /<%=([\s\S]+?)%>/g,
+                escape: /<%-([\s\S]+?)%>/g,
+                partial: /<%@([\s\S]+?)%>/g
+            },
+            processContent: function (src) {
+                return src;
+            },
+            separator: lf + lf
+        });
 
-    this.files.forEach(function(f) {
-      var output = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
-          return false;
-        } else {
-          return true;
-        }
-      })
-      .map(function(filepath) {
-        var src = options.processContent(grunt.file.read(filepath));
-        var compiled, filename;
+        var importPartial = function(source){
+        };
 
-        try {
-          compiled = _.template(src, false, options.templateSettings).source;
-        } catch (e) {
-          grunt.log.error(e);
-          grunt.fail.warn('JST ' + chalk.cyan(filepath) + ' failed to compile.');
-        }
+        // assign filename transformation functions
+        var processName = options.processName || defaultProcessName;
 
-        if (options.prettify) {
-          compiled = compiled.replace(/\n/g, '');
-        }
-        filename = processName(filepath);
-
-        if (options.amd && options.namespace === false) {
-          return 'return ' + compiled;
-        }
-        return nsInfo.namespace+'['+JSON.stringify(filename)+'] = '+compiled+';';
-      });
-
-      if (output.length < 1) {
-        grunt.log.warn('Destination not written because compiled files were empty.');
-      } else {
+        var nsInfo;
         if (options.namespace !== false) {
-          output.unshift(nsInfo.declaration);
+            nsInfo = lib.getNamespaceDeclaration(options.namespace);
         }
-        if (options.amd) {
-          if (options.prettify) {
-            output.forEach(function(line, index) {
-              output[index] = "  " + line;
-            });
-          }
-          output.unshift("define(function(){");
-          if (options.namespace !== false) {
-            // Namespace has not been explicitly set to false; the AMD
-            // wrapper will return the object containing the template.
-            output.push("  return " + nsInfo.namespace + ";");
-          }
-          output.push("});");
-        }
-        grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
-        grunt.log.writeln('File ' + chalk.cyan(f.dest) + ' created.');
-      }
-    });
 
-  });
+        files.forEach(function(f){
+            var sources = {};
+            var dest = f.dest;
+            f.src.forEach(function(filePath){
+                var templateName = options.processTemplateName(filePath);
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filePath)) {
+                    grunt.log.warn('Source file ' + chalk.cyan(filePath) + ' not found.');
+                    return;
+                }
+
+                if(templateName in sources){
+                    grunt.log.warn('More then one template are processed as same template name ' + chalk.cyan(templateName));
+                    return;
+                }
+
+                try {
+                    sources[templateName] = _.template(
+                        options.processContent(grunt.file.read(filePath)),
+                        false,
+                        options.templateSettings).source;
+                } catch (e) {
+                    grunt.log.error(e);
+                    grunt.fail.warn('JST ' + chalk.cyan(filePath) + ' failed to compile.');
+                }
+            });
+        });
+
+        files.forEach(function (f) {
+            var output = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file ' + chalk.cyan(filepath) + ' not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                var src = options.processContent(grunt.file.read(filepath));
+                var compiled, filename;
+
+                try {
+                    compiled = _.template(src, false, options.templateSettings).source;
+                } catch (e) {
+                    grunt.log.error(e);
+                    grunt.fail.warn('JST ' + chalk.cyan(filepath) + ' failed to compile.');
+                }
+
+                if (options.prettify) {
+                    compiled = compiled.replace(/\n/g, '');
+                }
+                filename = processName(filepath);
+
+                if (options.amd && options.namespace === false) {
+                    return 'return ' + compiled;
+                }
+                return nsInfo.namespace + '[' + JSON.stringify(filename) + '] = ' + compiled + ';';
+            });
+
+            if (output.length < 1) {
+                grunt.log.warn('Destination not written because compiled files were empty.');
+            } else {
+                if (options.namespace !== false) {
+                    output.unshift(nsInfo.declaration);
+                }
+                if (options.amd) {
+                    if (options.prettify) {
+                        output.forEach(function (line, index) {
+                            output[index] = "  " + line;
+                        });
+                    }
+                    output.unshift("define(function(){");
+                    if (options.namespace !== false) {
+                        // Namespace has not been explicitly set to false; the AMD
+                        // wrapper will return the object containing the template.
+                        output.push("  return " + nsInfo.namespace + ";");
+                    }
+                    output.push("});");
+                }
+                grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
+                grunt.log.writeln('File ' + chalk.cyan(f.dest) + ' created.');
+            }
+        });
+
+    });
 };
